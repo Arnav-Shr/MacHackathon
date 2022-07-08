028e9129b670a40c9eb760faab525c220dd13890
Arnav-Shr <arnv.shr@gmail.com>
2022-07-08 12:27:32 +1200

    Update algorithm.cs
    Half working not really lol

--- MacHackathon/Assets/Scripts/algorithm.cs
+++ MacHackathon/Assets/Scripts/algorithm.cs
@@ -7,7 +7,7 @@ public class algorithm : MonoBehaviour
 
     public void Start(){
         DateTime FinalAns = MatarikiDay(testYear);
-		Debug.Log(FinalAns.Date);
+		Debug.Log(FinalAns.Date + " Final Answer");
     }
 
 
@@ -21,17 +21,17 @@ public class algorithm : MonoBehaviour
         //Jan. 24, Wed 11:32 AM 1900 was a 3rd quarter start
         DateTime Date = new DateTime();
         for (int i = 19; i < 31; i++){ //check all days in june after the 19th
-            if (IsCorrectMoonPhase(new DateTime(year, 5, i))){
-                Date = new DateTime(year, 5, i);
+            if (IsCorrectMoonPhase(new DateTime(year, 6, i))){
+                Date = new DateTime(year, 6, i);
                 Date = FirstFriday(Date);
                 break;
             }
         }
 
-        if (Date.Month != 5){
+        if (Date.Month != 6){
             for (int i = 1; i < 32; i++){ //check all days in july
-                if (IsCorrectMoonPhase(new DateTime(year, 5, i))){
-                    Date = new DateTime(year, 6, i);
+                if (IsCorrectMoonPhase(new DateTime(year, 7, i))){
+                    Date = new DateTime(year, 7, i);
                     Date = FirstFriday(Date);
                     break;
                 }
@@ -49,7 +49,7 @@ public class algorithm : MonoBehaviour
         days += (Date.Year - 1900) * 365.2422;
         if ( DateTime.IsLeapYear(Date.Year)){
             days += 31 + 29 + 31 + 30 + 31; // jan feb mar apr may
-            if (Date.Month == 6){ //if month is june
+            if (Date.Month == 7){ //if month is june
                 days += Date.Day;
             } else { //if month is july
                 days += Date.Day + 30;
@@ -57,7 +57,7 @@ public class algorithm : MonoBehaviour
         }
         else {
             days += 31 + 28 + 31 + 30 + 31; // jan feb mar apr may
-            if (Date.Month == 6){ //if month is june
+            if (Date.Month == 7){ //if month is june
                 days += Date.Day;
             } else { //if month is july
                 days += Date.Day + 30;
@@ -79,30 +79,31 @@ public class algorithm : MonoBehaviour
     //finds closest friday to the Tangaroa Lunar Period
     static DateTime FirstFriday(DateTime Date)
     {
+        Debug.Log(Date.Date + "Tongoraro period Start");
         int counterPlus = 0;
         int counterMinus = 0;
 
         DateTime TempDatePlus = Date;
         //counting forward
-        while ((int) TempDatePlus.DayOfWeek != 4){
+        while ((int) TempDatePlus.DayOfWeek != 5){
             TempDatePlus = new DateTime(TempDatePlus.Year, TempDatePlus.Month, TempDatePlus.Day + 1);
             counterPlus += 1;
-            Debug.Log(counterPlus);
         }
-        //Debug.Log("now minus");
-        //DateTime TempDateMinus = Date;
-        ////counting backward
-        //while ((int) TempDateMinus.DayOfWeek != 4){
-        //    TempDatePlus = new DateTime(TempDatePlus.Year, TempDatePlus.Month, TempDatePlus.Day - 1);
-        //    counterMinus += 1;
-        //    Debug.Log(counterMinus);
-        //}
-//
-        //if (counterMinus > counterPlus){
+        Debug.Log(counterPlus);
+        Debug.Log("now minus");
+        DateTime TempDateMinus = Date;
+        //counting backward
+        while ((int) TempDateMinus.DayOfWeek != 5){
+            TempDatePlus = new DateTime(TempDatePlus.Year, TempDatePlus.Month, TempDatePlus.Day - 1);
+            counterMinus += 1;
+            Debug.Log(counterMinus);
+        }
+
+        if (counterMinus > counterPlus){
             return TempDatePlus;
-        //} 
-        //else {
-        //    return TempDateMinus;
-        //}     
+        } 
+        else {
+            return TempDateMinus;
+        }     
     }
 }
